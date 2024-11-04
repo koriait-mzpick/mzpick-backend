f2b43fb40dcef89ebd2412698417b1f2897c34f5
ha-myeongyun <97523237+devyunie@users.noreply.github.com>
2024-11-04 16:17:03 +0900

    1234

--- src/main/java/com/koreait/mzpick_backend/dto/response/hallOfFame/GetFashionHallOfFameResponseDto.java
+++ src/main/java/com/koreait/mzpick_backend/dto/response/hallOfFame/GetFashionHallOfFameResponseDto.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.koreait.mzpick_backend.common.object.fashion.Fashion;
 import com.koreait.mzpick_backend.dto.response.ResponseCode;
 import com.koreait.mzpick_backend.dto.response.ResponseDto;
 import com.koreait.mzpick_backend.dto.response.ResponseMessage;
@@ -14,11 +15,14 @@ import lombok.Getter;
 
 @Getter
 public class GetFashionHallOfFameResponseDto extends ResponseDto{
-    private GetFashionHallOfFamePhotoListResultSet resultSet;
+    private Integer fashionNumber;
+    private String photoLink; 
+
 
     public GetFashionHallOfFameResponseDto(List<GetFashionHallOfFamePhotoListResultSet> resultSet) {
         super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
-        this.resultSet = resultSet.get(0);
+        this.fashionNumber = resultSet.get(0).getFashionNumber();
+        this.photoLink = resultSet.get(0).getPhotoLink();
     }
 
     public static ResponseEntity<GetFashionHallOfFameResponseDto> success(List<GetFashionHallOfFamePhotoListResultSet> resultSet){
